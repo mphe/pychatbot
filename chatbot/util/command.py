43b83dad62ef2f5a4b8735c400674a34e2486ff7
Marvin Ewald <marvin.e@protonmail.ch>
2016-11-27 22:02:54 +0100

    Revise CommandHandler
    - Decouple from APIEventDispatcher
    - Use exceptions

--- chatbot/util/command.py
+++ chatbot/util/command.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import traceback
 import logging
 import shlex
-from collections import namedtuple
-from .. import api
 
 
 # Command flags
@@ -33,15 +30,24 @@ COMMAND_ERR_SYNTAX      = 4 # Syntax error
 COMMAND_ERR_NOTFOUND    = 5 # Continue as if the command didn't exist
                             # (useful in COMMAND_MISSING callbacks)
 
-# Error messages
-COMMAND_ERROR_STRINGS = {
-    COMMAND_SUCCESS         : "Success",
-    COMMAND_ERR             : "An error occurred",
-    COMMAND_ERR_ARGC        : "Wrong number of arguments",
-    COMMAND_ERR_PERM        : "Permission denied",
-    COMMAND_ERR_SYNTAX      : "Syntax error",
-    COMMAND_ERR_NOTFOUND    : "Command not found"
-}
+class CommandError(Exception):
+    _err_strings = {
+        COMMAND_SUCCESS         : "Success",
+        COMMAND_ERR             : "An error occurred",
+        COMMAND_ERR_ARGC        : "Wrong number of arguments",
+        COMMAND_ERR_PERM        : "Permission denied",
+        COMMAND_ERR_SYNTAX      : "Syntax error",
+        COMMAND_ERR_NOTFOUND    : "Command not found"
+    }
+
+    def __init__(self, code, command):
+        super(CommandError, self).__init__(CommandError._err_strings[code])
+        self.code = code
+        self.command = command
+
+    def __repr__(self):
+        return "CommandError({}, {})".format(repr(self.code), repr(self.command))
+
 
 class Command(object):
     def __init__(self, callback, argc, flags):
@@ -55,38 +61,39 @@ class Command(object):
 
 
 class CommandHandler(object):
-    def __init__(self, nofail=True, prefix=[ "!bot ", "!", "@bot " ], admins=[]):
+    def __init__(self, prefix=[ "!" ], admins=[]):
         self._prefix = prefix
         self._admins = admins
         self._cmds = {}
-        self._evhandle = None
-        self._nofail = nofail
-
-    def attach(self, dispatcher):
-        if not self._evhandle:
-            self._evhandle = dispatcher.register(api.APIEvents.Message,
-                                                 self._on_msg_receive)
-            self.register("help", self._help, argc=0)
-            self.register("list", self._list, argc=0)
-
-    def detach(self):
-        if self._evhandle:
-            self._evhandle.unregister()
-            self._evhandle = None
+        # self._evhandle = None
+        self.register("help", self._help, argc=0)
+        self.register("list", self._list, argc=0)
 
     def register(self, name, callback, argc=1, flags=0):
         """Register a chat command.
         
-        The callback must be in the format:
+        The callback must be of the format:
             (api.ChatMessage, list[str]) -> int/None
         or
             (api.ChatMessage, *args) -> int/None
 
-        The first argument to the callback is the message object issuing
-        the command. The second argument is a list of arguments to the
-        command (the first element is the name of the command itself).
+        The first argument is the message object issuing the command.
+        The second argument is the list of arguments that were passed to
+        the command (the first element is the name of the command itself).
 
-        The return value indicates if the command was successful or failed.
+        There are 2 ways of letting a command fail: raising an exception or
+        returning an error code.
+
+        Aside from normal exceptions, there is a CommandError exception
+        that can be raised to indicate a command specific error.
+        It needs an error code and the name of the command to be constructed,
+        e.g.: raise CommandError(COMMAND_ERR_PERM, "mycommand").
+
+        Returning an error code basically leads to the same outcome.
+        If an error code (except COMMAND_SUCCESS) is returned, an exception
+        like above will be raised with this specific error code.
+        Returning nothing (None) is equal to returning COMMAND_SUCCESS.
+
         Possible values are:
             COMMAND_SUCCESS       -  Success, no error
             COMMAND_ERR           -  Unspecified error
@@ -94,8 +101,6 @@ class CommandHandler(object):
             COMMAND_ERR_PERM      -  Missing permissions
             COMMAND_ERR_SYNTAX    -  Syntax error
             COMMAND_ERR_NOTFOUND  -  Continue as if the command didn't exist
-
-        Returning nothing (None) is equal to returning COMMAND_SUCCESS.
 
         There is one special command called COMMAND_MISSING, which is called
         when a command was not found.
@@ -120,57 +125,46 @@ class CommandHandler(object):
         else:
             logging.warn("Trying to unregister non-existing command: " + name)
 
-    def _on_msg_receive(self, msg):
+    def execute(self, msg):
+        """Execute a command from a message."""
         argv = []
 
+        command = msg.get_text()
         # Check if the message is a command and fill argv with its arguments.
         for i in self._prefix:
-            if msg.get_text().startswith(i):
-                argv = shlex.split(msg.get_text()[len(i):])
-                # Abort if no command supplied or when the command is another
-                # prefix.
-                if not argv or argv[0].startswith(i.strip()):
-                    return
+            if command.startswith(i):
+                # Abort if the command is another prefix
+                if not command[len(i):].strip().startswith(i.strip()):
+                    argv = shlex.split(command[len(i):])
                 break
 
-        if not argv:
-            return
+        if argv:
+            cmd = None
+            if self._cmds.has_key(argv[0]):
+                cmd = self._cmds[argv[0]]
+            elif self._cmds.has_key(COMMAND_MISSING):
+                cmd = self._cmds[COMMAND_MISSING]
 
-        cmd = None
-        if self._cmds.has_key(argv[0]):
-            cmd = self._cmds[argv[0]]
-        elif self._cmds.has_key(COMMAND_MISSING):
-            cmd = self._cmds[COMMAND_MISSING]
+            ret = self._exec_command(msg, cmd, argv)
 
-        ret = self._exec_command(cmd, msg, argv)
+            if ret != COMMAND_SUCCESS:
+                raise CommandError(ret, argv[0])
 
-        if ret != COMMAND_SUCCESS:
-            msg.get_chat().send_message( "Error: " + COMMAND_ERROR_STRINGS[ret])
-
-    def _exec_command(self, cmd, msg, argv):
+    def _exec_command(self, msg, cmd, argv):
         if not cmd:
             return COMMAND_ERR_NOTFOUND
 
         if cmd.flags & CMDFLAG_ADMIN:
-            if msg.author_handle() not in self._admins:
+            if msg.get_author().handle() not in self._admins:
                 return COMMAND_ERR_PERM
 
         if len(argv) <= cmd.argc:
             return COMMAND_ERR_ARGC
 
-        try:
-            if cmd.flags & CMDFLAG_EXPAND:
-                return cmd(msg, *argv)
-            else:
-                return cmd(msg, argv)
-        except Exception as e:
-            if self._nofail:
-                logging.error(traceback.format_exc())
-                msg.get_chat().send_message("Error: " + str(e))
-                # Hide the exception, it has already been logged
-                return COMMAND_SUCCESS
-            else:
-                raise
+        if cmd.flags & CMDFLAG_EXPAND:
+            return cmd(msg, *argv)
+        else:
+            return cmd(msg, argv)
 
 
     # Commands
--- tests/command_test.py
+++ tests/command_test.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from context import *
+from functools import partial
 
 running = True
 
@@ -27,22 +28,28 @@ def cmd_missing(msg, self, *args):
 def on_sent(msg):
     logging.info(str(msg))
 
+def on_receive(cmd_handler, msg):
+    try:
+        cmd_handler.execute(msg)
+    except Exception as e:
+        logging.error(str(e))
+
 
 def main():
     logging.info("Creating API object")
     apiobj = api.create_api_object("test", stub=False, interactive=True)
 
-    disp = util.event.APIEventDispatcher(apiobj)
-    disp.register(api.APIEvents.MessageSent, on_sent)
     cmd = util.command.CommandHandler()
-    cmd.attach(disp)
-
     cmd.register("exit", cmd_exit, argc=0)
     cmd.register("echo", cmd_echo)
     cmd.register("fail", cmd_fail, argc=0)
     cmd.register("admin", cmd_admin, argc=0, flags=util.command.CMDFLAG_ADMIN)
     cmd.register(util.command.COMMAND_MISSING, cmd_missing, argc=0,
                  flags=util.command.CMDFLAG_EXPAND)
+
+    disp = util.event.APIEventDispatcher(apiobj)
+    disp.register(api.APIEvents.MessageSent, on_sent)
+    disp.register(api.APIEvents.Message, partial(on_receive, cmd))
 
     logging.info("Attaching...")
     apiobj.attach()
@@ -51,7 +58,6 @@ def main():
         apiobj.iterate()
 
     logging.info("Detaching...")
-    cmd.detach()
     disp.clear()
     apiobj.detach()
     logging.info("Done")
